30d9e4d8b4f8822bf722fb2504f131e38c547380
wwolf <github.boasting734@passmail.com>
2026-09-09 12:10:42 -0400

    Select Working
    Figured out how to make a predicate with a lambda expression. Used this to show a working example of select.

--- src/main/java/uga/csx370/mydbimpl/Driver.java
+++ src/main/java/uga/csx370/mydbimpl/Driver.java
@@ -10,7 +10,10 @@ import java.util.List;
 
 import uga.csx370.mydb.Relation;
 import uga.csx370.mydb.RelationBuilder;
+import uga.csx370.mydb.Predicate;
 import uga.csx370.mydb.Type;
+import uga.csx370.mydb.Cell;
+import uga.csx370.mydb.RA;
 
 public class Driver {
 
@@ -21,23 +24,41 @@ public class Driver {
         // After creating the table, data is loaded from a CSV file.
         // Path should be replaced with a correct file path for a compatible
         // CSV file.
+        /*
         Relation rel1 = new RelationBuilder()
                 .attributeNames(List.of("Col01_Name", "Col02_Name", "Col03_Name"))
                 .attributeTypes(List.of(Type.INTEGER, Type.STRING, Type.DOUBLE))
                 .build();
+        */
 
+        /*
         Relation rel2 = new RelationBuilder()
                 .attributeNames(List.of("ID", "Name", "Dept_Name", "Salary"))
                 .attributeTypes(List.of(Type.STRING, Type.STRING, Type.STRING, Type.DOUBLE))
                 .build();
+        rel2.loadData("data/instructor_export.csv");
+        rel2.print();
+        */
+
         Relation rel3 = new RelationBuilder()
                 .attributeNames(List.of("course_id", "title", "dept_name", "credits"))
                 .attributeTypes(List.of(Type.STRING, Type.STRING, Type.STRING, Type.INTEGER))
                 .build();
-        rel2.loadData("data/instructor_export.csv");
         rel3.loadData("data/course_export.csv");
-        rel2.print();
         rel3.print();
+        Predicate deptPhysics = row -> {
+          // Row is a list of cells, so getting an index gets the specific cell value
+          int col = rel3.getAttrIndex("dept_name");
+          Cell deptCell = row.get(col);
+          String dept = deptCell.getAsString();
+          // Return equality value
+          return dept.equals("Physics");
+        };
+
+        // Needed instance of RA
+        RA ra = new RAImpl();
+        Relation physicsCourses = ra.select(rel3, deptPhysics);
+        physicsCourses.print();
     }
 
 }
